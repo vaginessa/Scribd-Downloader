6f9e86934177ebee56f7cbb2cee28ca9f3fb1cf3
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2018-11-23 10:51:15 +0530

    Make use of inheritance

--- scribdl/scribdl.py
+++ scribdl/scribdl.py
@@ -9,6 +9,7 @@ import shutil
 import sys
 import json
 import argparse
+from abc import ABC, abstractmethod
 
 
 def get_arguments():
@@ -57,70 +58,23 @@ def sanitize_title(title):
 
     return title
 
+class ScribdBase(ABC):
+    @abstractmethod
+    def get_content(self):
+        pass
 
-class ScribdDocument:
-    def __init__(self, url, image_document):
+class ScribdDocument(ScribdBase):
+    def __init__(self, url):
         self.url = url
-        self.image_document = image_document
-        self.images_list = []
         response = requests.get(url)
         self.soup = BeautifulSoup(response.text, 'html.parser')
-
-    def get_document(self):
-        title = self.get_title()
-        js_text = self.soup.find_all('script', type='text/javascript')
-        jsonp_urls = self._extract_all_jsonp_urls(js_text)
-
-        if self.image_document:
-            # sometimes images embedded directly in html as well
-            return self.image_extractor(jsonp_urls, title)
-        else:
-            print('Extracting text to ' + title + '.md\n')
-            filename = title + '.md'
-            self.text_extractor(jsonp_urls, filename)
-            return filename
 
     def get_title(self):
         title = self.soup.find('title').get_text()
         return sanitize_title(title)
 
-    def _html_image_extractor(self, initial_filename):
-        downloaded_images = []
-        absimg = self.soup.find_all('img', {'class':'absimg'}, src=True)
-        for img in absimg:
-            filename = '{}_{}.jpg'.format(initial_filename,
-                                          len(downloaded_images) + 1)
-            self.save_image(img['src'], filename, found=False)
-            downloaded_images.append(filename)
-        return downloaded_images
-
-    def image_extractor(self, jsonp_urls, initial_filename):
-        downloaded_images = self._html_image_extractor(initial_filename)
-        found = len(downloaded_images) > 0
-        for jsonp_url in jsonp_urls[:2]:
-            filename = '{}_{}.jpg'.format(initial_filename,
-                                          len(downloaded_images) + 1)
-            self.save_image(jsonp_url, filename, found)
-            downloaded_images.append(filename)
-        return downloaded_images
-
-    def text_extractor(self, jsonp_urls, filename):
-        for jsonp_url in jsonp_urls:
-            self._save_text(jsonp_url, filename)
-
-    def convert_to_image_url(self, url, found):
-        if url.endswith('.jsonp'):
-            replacement = url.replace('/pages/', '/images/')
-            if found:
-                replacement = replacement.replace('.jsonp', '/000.jpg')
-            else:
-                replacement = replacement.replace('.jsonp', '.jpg')
-        else:
-            replacement = url
-
-        return replacement
-
-    def _extract_all_jsonp_urls(self, js_text):
+    def _extract_all_jsonp_urls(self):
+        js_text = self.soup.find_all('script', type='text/javascript')
         jsonp_urls = []
         for opening in js_text:
             for inner_opening in opening:
@@ -140,18 +94,30 @@ class ScribdDocument:
 
         return jsonp
 
-    def save_image(self, jsonp_url, imagename, found=False):
-        print("Downloading " + imagename)
-        already_present = os.listdir('.')
-        if imagename in already_present:
-            return
+    @abstractmethod
+    def get_content(self):
+        pass
 
-        url = self.convert_to_image_url(jsonp_url, found)
-        response = requests.get(url, stream=True)
-        with open(imagename, 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
 
-    def _save_text(self, jsonp, filename):
+class ScribdTextualDocument(ScribdDocument):
+    def get_content(self):
+        title = self.get_title()
+        jsonp_urls = self._extract_all_jsonp_urls()
+
+        print('Extracting text to ' + title + '.md\n')
+        filename = title + '.md'
+        self.text_extractor(jsonp_urls, filename)
+        return filename
+
+    def get_title(self):
+        title = self.soup.find('title').get_text()
+        return sanitize_title(title)
+
+    def text_extractor(self, jsonp_urls, filename):
+        for jsonp_url in jsonp_urls:
+            self.save_text(jsonp_url, filename)
+
+    def save_text(self, jsonp, filename):
         response = requests.get(jsonp).text
         page_no = response[11:12]
 
@@ -170,7 +136,63 @@ class ScribdDocument:
                 feed.write(extraction)
 
 
-class ScribdBook:
+class ScribdImageDocument(ScribdDocument):
+    def get_content(self):
+        title = self.get_title()
+        jsonp_urls = self._extract_all_jsonp_urls()
+
+        # sometimes images embedded directly in html as well
+        return self.image_extractor(jsonp_urls, title)
+
+    def get_title(self):
+        title = self.soup.find('title').get_text()
+        return sanitize_title(title)
+
+    def image_extractor(self, jsonp_urls, initial_filename):
+        downloaded_images = self._html_image_extractor(initial_filename)
+        found = len(downloaded_images) > 0
+        for jsonp_url in jsonp_urls:
+            filename = '{}_{}.jpg'.format(initial_filename,
+                                          len(downloaded_images) + 1)
+            self.save_image(jsonp_url, filename, found)
+            downloaded_images.append(filename)
+        return downloaded_images
+
+    def _html_image_extractor(self, initial_filename):
+        downloaded_images = []
+        absimg = self.soup.find_all('img', {'class':'absimg'}, src=True)
+        for img in absimg:
+            filename = '{}_{}.jpg'.format(initial_filename,
+                                          len(downloaded_images) + 1)
+            self.save_image(img['src'], filename, found=False)
+            downloaded_images.append(filename)
+        return downloaded_images
+
+    def convert_to_image_url(self, url, found):
+        if url.endswith('.jsonp'):
+            replacement = url.replace('/pages/', '/images/')
+            if found:
+                replacement = replacement.replace('.jsonp', '/000.jpg')
+            else:
+                replacement = replacement.replace('.jsonp', '.jpg')
+        else:
+            replacement = url
+
+        return replacement
+
+    def save_image(self, jsonp_url, imagename, found=False):
+        print("Downloading " + imagename)
+        already_present = os.listdir('.')
+        if imagename in already_present:
+            return
+
+        url = self.convert_to_image_url(jsonp_url, found)
+        response = requests.get(url, stream=True)
+        with open(imagename, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+
+
+class ScribdBook(ScribdBase):
     def __init__(self, url):
         self.url = url
 
@@ -185,7 +207,7 @@ class ScribdBook:
                 words += self._extract_text(word)
         return words
 
-    def get_book(self):
+    def get_content(self):
         book_id = str(self.get_id())
         token = self._get_token(book_id)
 
@@ -222,7 +244,7 @@ class ScribdBook:
 
             if block['type'] in ('text', 'image'):
                 print(string_text)
-                self._save_text(string_text, filename)
+                self.save_text(string_text, filename)
 
     def _format_content_url(self, book_id, chapter, token):
         unformatted_url = ('https://www.scribd.com/scepub/{}/chapters/{}/'
@@ -248,7 +270,7 @@ class ScribdBook:
         token = requests.post(token_url)
         return json.loads(token.text)['response']
 
-    def _save_text(self, string_text, filename):
+    def save_text(self, string_text, filename):
             with open(filename, 'a') as f:
                 f.write(string_text)
 
@@ -295,13 +317,17 @@ class Downloader:
 
     def _download_book(self):
         book = ScribdBook(self.url)
-        md_path = book.get_book()
+        md_path = book.get_content()
         pdf_path = '{}.pdf'.format(book.get_id())
         return ConvertToPDF(md_path, pdf_path)
 
     def _download_document(self, image_document):
-        document = ScribdDocument(self.url, image_document)
-        content_path = document.get_document()
+        if image_document:
+            document = ScribdImageDocument(self.url)
+        else:
+            document = ScribdTextualDocument(self.url)
+
+        content_path = document.get_content()
         pdf_path = '{}.pdf'.format(document.get_title())
         return ConvertToPDF(content_path, pdf_path)
 
