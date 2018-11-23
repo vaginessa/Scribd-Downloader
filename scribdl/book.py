66c7dcbf9df796745780807ed6f5739b933cc757
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2018-11-23 12:12:28 +0530

    Split code into modules

---
+++ scribdl/book.py
@@ -0,0 +1,123 @@
+import requests
+import json
+
+from .base import ScribdBase
+
+
+class ScribdBook(ScribdBase):
+    """
+    A class for downloading books off Scribd.
+
+    Parameters
+    ----------
+    url : `str`
+        A string containing Scribd book URL.
+    """
+
+    def __init__(self, url):
+        self.url = url
+
+    def _extract_text(self, content):
+        """
+        Extracts text given a block of raw html.
+        """
+        words = []
+        for word in content['words']:
+            if word.get('break_map', None):
+                words.append(word['break_map']['text'])
+            elif word.get('text', None):
+                words.append(word['text'])
+            else:
+                words += self._extract_text(word)
+        return words
+
+    def get_content(self):
+        """
+        Processing text and image extraction.
+        """
+        book_id = str(self.get_id())
+        token = self._get_token(book_id)
+
+        filename = book_id + '.md'
+        chapter = 1
+
+        while True:
+            url = self._format_content_url(book_id, chapter, token)
+            response = requests.get(url)
+
+            try:
+                json_response = json.loads(response.text)
+                self._extract_text_blocks(json_response,
+                                          book_id,
+                                          chapter,
+                                          token,
+                                          filename)
+                chapter += 1
+
+            except ValueError:
+                print('No more content being exposed by Scribd!')
+                break
+
+        return filename
+
+    def _extract_text_blocks(self, response_dict, book_id, chapter, token, filename):
+        """
+        Extracts small blocks of raw book text and image
+        URLs and writes them to a file.
+        """
+        for block in response_dict['blocks']:
+            if block['type'] == 'text':
+                string_text = ' '.join(self._extract_text(block)) + '\n\n'
+            elif block['type'] == 'image':
+                image_url = self._format_image_url(book_id, chapter, block['src'], token)
+                imagename = block['src'].replace('images/', '')
+                string_text = '![{}]({})\n\n'.format(imagename, image_url)
+
+            if block['type'] in ('text', 'image'):
+                print(string_text)
+                self.save_text(string_text, filename)
+
+    def _format_content_url(self, book_id, chapter, token):
+        """
+        Generates a string which points to a URL containing
+        the raw book text.
+        """
+        unformatted_url = ('https://www.scribd.com/scepub/{}/chapters/{}/'
+                          'contents.json?token={}')
+        return unformatted_url.format(book_id, chapter, token)
+
+    def _format_image_url(self, book_id, chapter, image, token):
+        """
+        Generates a string which points to an image URL.
+        """
+        unformatted_url = ('https://www.scribd.com/scepub/{}/chapters/{}/'
+                          '{}?token={}')
+        return unformatted_url.format(book_id, chapter, image, token)
+
+    def get_id(self):
+        """
+        Extracts the book ID.
+        """
+        splits = self.url.split('/')
+        for split in splits:
+            try:
+                book_id = int(split)
+            except ValueError:
+                continue
+        return book_id
+
+    def _get_token(self, book_id):
+        """
+        Fetches a uniquely generated token for the current
+        session.
+        """
+        token_url = 'https://www.scribd.com/read2/{}/access_token'.format(book_id)
+        token = requests.post(token_url)
+        return json.loads(token.text)['response']
+
+    def save_text(self, string_text, filename):
+        """
+        Writes text to the passed file.
+        """
+        with open(filename, 'a') as f:
+            f.write(string_text)
---
+++ scribdl/downloader.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+import requests
+
+from .document import ScribdTextualDocument
+from .document import ScribdImageDocument
+from .book import ScribdBook
+
+from .pdf_converter import ConvertToPDF
+
+
+class Downloader:
+    """
+    A helper class for downloading books and documents off Scribd.
+
+    Parameters
+    ----------
+    url : `str`
+        A string containing path to a Scribd URL
+    """
+
+    def __init__(self, url):
+        self.url = url
+        self._is_book = self.is_book()
+
+    def download(self, is_image_document=None):
+        """
+        Downloads books and documents from Scribd.
+        Returns an object of `ConvertToPDF` class.
+        """
+        if self._is_book:
+            content = self._download_book()
+        else:
+            if is_image_document is None:
+                raise TypeError("The input URL points to a document. You must specify "
+                                "whether it is an image document or a textual document "
+                                "in the `image_document` parameter.")
+            content = self._download_document(is_image_document)
+
+        return content
+
+    def _download_book(self):
+        """
+        Downloads books off Scribd.
+        Returns an object of `ConvertToPDF` class.
+        """
+        book = ScribdBook(self.url)
+        md_path = book.get_content()
+        pdf_path = '{}.pdf'.format(book.get_id())
+        return ConvertToPDF(md_path, pdf_path)
+
+    def _download_document(self, image_document):
+        """
+        Downloads textual and image documents off Scribd.
+        Returns an object of `ConvertToPDF` class.
+        """
+        if image_document:
+            document = ScribdImageDocument(self.url)
+        else:
+            document = ScribdTextualDocument(self.url)
+
+        content_path = document.get_content()
+        pdf_path = '{}.pdf'.format(document.get_title())
+        return ConvertToPDF(content_path, pdf_path)
+
+    def is_book(self):
+        """
+        Checks whether the passed URL points to a Scribd book
+        or a Scribd document
+        """
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        content_class = soup.find('body')['class']
+        matches_with_book = content_class[0] == "autogen_class_views_layouts_book_web"
+        return matches_with_book
