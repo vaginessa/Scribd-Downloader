ffaa971682da62be20de0509a04725166293f285
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2019-01-27 22:59:53 +0530

    Add audiobook test

--- test/test_download.py
+++ test/test_download.py
@@ -9,15 +9,12 @@ def cwd_to_tmpdir(tmpdir):
     os.chdir(str(tmpdir))
 
 
-def test_book_download(cwd_to_tmpdir, monkeypatch):
-    book_url = "https://www.scribd.com/read/262694921/Acting-The-First-Six-Lessons"
-    book_downloader = Downloader(book_url)
-    # We don't want to clutter stdout with book contents if this test fails
-    monkeypatch.setattr("builtins.print", lambda x: None)
-    md_book = book_downloader.download()
-    assert os.path.getsize(md_book.input_content) in range(10000, 20000)
-    md_book.to_pdf()
-    assert os.path.getsize(md_book.pdf_path) in range(200000, 2500000)
+def test_audiobook_download(cwd_to_tmpdir, monkeypatch):
+    audiobook_url = "https://www.scribd.com/audiobook/237606860/100-Ways-to-Motivate-Yourself-Change-Your-Life-Forever"
+    audiobook_downloader = Downloader(audiobook_url)
+    audio = audiobook_downloader.download()
+    assert audio[0] == "100_Ways_to_Motivate_Yourself_Change_Your_Life_Forever_preview.mp3"
+    assert os.path.getsize(audio[0]) == 2127830
 
 
 def test_text_document_download(cwd_to_tmpdir):
@@ -36,3 +33,15 @@ def test_img_document_download(cwd_to_tmpdir):
     assert len(imgs.input_content) == 2
     imgs.to_pdf()
     assert os.path.getsize(imgs.pdf_path) in range(140000, 150000)
+
+
+def test_book_download(cwd_to_tmpdir, monkeypatch):
+    book_url = "https://www.scribd.com/read/262694921/Acting-The-First-Six-Lessons"
+    book_downloader = Downloader(book_url)
+    # We don't want to clutter stdout with book contents if this test fails
+    monkeypatch.setattr("builtins.print", lambda x: None)
+    md_book = book_downloader.download()
+    assert os.path.getsize(md_book.input_content) in range(10000, 20000)
+    md_book.to_pdf()
+    assert os.path.getsize(md_book.pdf_path) in range(200000, 2500000)
+
